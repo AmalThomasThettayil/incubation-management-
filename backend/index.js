58dab26752fc5381b9d52d963a7be3cdf0141619
AmalThomasThettayil <102983661+AmalThomasThettayil@users.noreply.github.com>
2022-08-22 17:44:36 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require ("express")
+const app  = express()
+const cors = require ("cors")
+const dotenv = require("dotenv")
+const connectDatabase = require('./config/connection')
+const userRoutes=require('./routes/user')
+const adminRoutes = require('./routes/admin')
+
+dotenv.config();
+
+// const userRoutes = require('./routes/user')
+
+
+connectDatabase()
+
+app.use(cors({
+    origin:['http://localhost:3000'],
+    methods:['GET',"POST","PUT","PATCH"],
+    credentials:true
+}));
+app.use(express.json())
+app.use('/',userRoutes)
+app.use('/admin',adminRoutes)
+
+app.listen(4000,()=>{
+    console.log("Server started on port 4000");
+})
+
